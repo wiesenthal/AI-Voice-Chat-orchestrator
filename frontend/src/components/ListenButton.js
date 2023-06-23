7bb23d270f38d944d8c0683a3ad9bd5fa9846d8e
Miles Wiesenthal <mileswiesenthal@gmail.com>
2023-06-23 15:09:00 -0700

    component TalkToIt

--- frontend/src/components/ListenButton.js
+++ frontend/src/components/ListenButton.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
+import './ListenButton.css';
+
+
 const ListenButton = ({ onStartListening, onStopListening }) => {
   const [isActive, setIsActive] = useState(false);
 
